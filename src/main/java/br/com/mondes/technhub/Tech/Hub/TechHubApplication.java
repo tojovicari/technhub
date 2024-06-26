7e0edd63db30892bc3e82886ecb054506afce0c6
TojoVicari <glauber.vicari@outlook.com>
2024-06-26 14:45:29 -0300

    Commit inicial

---
+++ src/main/java/br/com/mondes/technhub/Tech/Hub/TechHubApplication.java
@@ -0,0 +1,13 @@
+package br.com.mondes.technhub.Tech.Hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mondes/technhub/Tech/Hub/TechHubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mondes.technhub.Tech.Hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
